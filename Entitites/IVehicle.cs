2789a7f53d79d9be4964a175531d9363ce1928d2
marcus pedersen <marcus@mpedersen.me>
2024-06-17 08:14:27 +0200

    Created base class Vehicle & interface IVehicle

---
+++ Entitites/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Entitites
+{
+    public interface IVehicle
+    {
+        string RegistrationNumber { get; set; }
+        string Color { get; set; }
+        int NumberOfWheels { get; set; }
+    }
+}
---
+++ Entitites/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Entitites
+{
+    public class Vehicle : IVehicle
+    {
+        public string RegistrationNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Color { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int NumberOfWheels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
