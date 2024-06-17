1d03d4e837edfcd702c01f0f3d55772460e2e057
marcus pedersen <marcus@mpedersen.me>
2024-06-17 23:08:38 +0200

    Add some comments to IGarage

--- Interfaces/IGarage.cs
+++ Interfaces/IGarage.cs
@@ -2,10 +2,10 @@
 {
     public interface IGarage<T> : IEnumerable<T> where T : IVehicle
     {
-        void Park(T vehicle);
-        bool Remove(T vehicle);
-        T Find(string registrationNumber);
-        int Capacity { get; }
-        int Count { get; }
+        void Park(T vehicle); // Adds a vehicle to the garage
+        bool Remove(T vehicle); // Removes a vehicle from the garage
+        T Find(string registrationNumber); // Finds a vehicle by its registration number
+        int Capacity { get; } // Maximum number of vehicles the garage can hold
+        int Count { get; } // Current number of vehicles in the garage
     }
 }
