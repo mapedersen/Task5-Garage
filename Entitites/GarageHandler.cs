664a3696074cbba5ac14aa492249b13788465574
marcus pedersen <marcus@mpedersen.me>
2024-06-17 19:50:06 +0200

    Implement GarageHandler

--- Entitites/GarageHandler.cs
+++ Entitites/GarageHandler.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Task5.App.Interfaces;
 
 namespace Task5.App.Entitites
 {
-    internal class GarageHandler
+    internal class GarageHandler<T> : IHandler<T> where T : IVehicle
     {
+        private readonly IGarage<T> _garage;
+        public GarageHandler(IGarage<T> garage)
+        {
+            _garage = garage;
+        }
+        public void AddVehicle(T vehicle)
+        {
+            _garage.Park(vehicle);
+        }
+
+        public bool RemoveVehicle(string registrationNumber)
+        {
+            T vehicle = _garage.Find(registrationNumber);
+            if (vehicle != null)
+            {
+                return _garage.Remove(vehicle);
+            }
+            return false;
+        }
+
+        public T GetVehicle(string registrationNumber)
+        {
+            return _garage.Find(registrationNumber);
+        }
+
+        public IEnumerable<T> GetAllVehicles()
+        {
+            return _garage;
+        }
     }
 }
